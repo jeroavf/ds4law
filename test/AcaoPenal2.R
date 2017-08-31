2c978e664e25c5d981c7e12cfeee9611e4aacde6
Jeronimo Avelar Filho <jeronimo@mpf.mp.br>
2017-08-31 14:56:04 -0300

    Mudanca nos tipos de dados das colunas para factor e data onde necessário

--- test/AcaoPenal2.R
+++ test/AcaoPenal2.R
@@ -4,6 +4,7 @@ library(listviewer)
 ###
 library(tidyr)
 library(dplyr)
+library(lubridate)
 
 ###
 #library(tidyjson)
@@ -26,5 +27,13 @@ colnames(processos_flat ) <- c("numero_processo", "classe_processo", "numero_pro
                                "origem", "estado", "tipo", "nome_instituicao", "idpartes", 
                                "ordem", "nome", "classe_parte"  )
 names(processos_flat)
+# acerta tipo factor 
 filter(processos_flat , numero_processo == "4760")
-
+processos_flat$classe_parte <- factor(processos_flat$classe_parte )
+processos_flat$nome_instituicao <- factor(processos_flat$nome_instituicao )
+processos_flat$tipo <- factor(processos_flat$tipo )
+processos_flat$estado <- factor(processos_flat$estado )
+processos_flat$origem <- factor(processos_flat$origem )
+processos_flat$classe_processo <- factor(processos_flat$classe_processo )
+processos_flat$data_protocolo <- ymd_hms(processos_flat$data_protocolo )
+glimpse(processos_flat)
